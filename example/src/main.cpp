7610404c6b01229e49f071ce6fae7bd2e512b715
Andrey Potemkin <appanpotemkin@gmail.com>
2019-02-17 04:29:16 +0300

    tabs -> spaces in example

--- example/src/main.cpp
+++ example/src/main.cpp
@@ -5,53 +5,53 @@
 #include <stddef.h>
 #include <vector>
 
-int main()
-{
+int main() {
 
-	auto devices = cl_graph::Device::get_all_devices();
-	const auto it = std::find_if(devices.begin(), devices.end(), [] (const auto & d) { return d.get_type() == cl_graph::Device::CPU; });
-	if (it == devices.end()) {
-		std::cerr << "No needed device" << std::endl;
-		return -1;
-	}
-	auto device = *it;
-	// construct graph
-	cl_graph::Data data1 = cl_graph::Data({ 1.0f, -2.0f, 3.0f, 4.0f }, { 2, 2 });
-	cl_graph::Data data2 = cl_graph::Data({ 5.0f, 6.0f, -7.0f, 8.0f }, { 2, 2 });
-	cl_graph::Data scalar = cl_graph::Data(9.0);
+    auto devices = cl_graph::Device::get_all_devices();
+    const auto it = std::find_if(devices.begin(), devices.end(),
+                                 [](const auto & d) { return d.get_type() == cl_graph::Device::CPU; });
+    if (it == devices.end()) {
+        std::cerr << "No needed device" << std::endl;
+        return -1;
+    }
+    auto device = *it;
+    // construct graph
+    cl_graph::Data data1 = cl_graph::Data({1.0f, -2.0f, 3.0f, 4.0f}, {2, 2});
+    cl_graph::Data data2 = cl_graph::Data({5.0f, 6.0f, -7.0f, 8.0f}, {2, 2});
+    cl_graph::Data scalar = cl_graph::Data(9.0);
 
-	auto add_node = cl_graph::Node::add_node(data1, data2, device);
+    auto add_node = cl_graph::Node::add_node(data1, data2, device);
     auto add_scalar = cl_graph::Node::add_node(add_node, scalar, device);
-	// get result
-	auto add_result = add_scalar.evaluate();
+    // get result
+    auto add_result = add_scalar.evaluate();
 
-	// output result
-	std::cout << "A matrix:\n" << data1 << '\n';
-	std::cout << "+\n";
-	std::cout << "B matrix:\n" << data2 << '\n';
-	std::cout << "=\n";
-	std::cout << "C matrix:\n" << add_result << "\n\n";
+    // output result
+    std::cout << "A matrix:\n" << data1 << '\n';
+    std::cout << "+\n";
+    std::cout << "B matrix:\n" << data2 << '\n';
+    std::cout << "=\n";
+    std::cout << "C matrix:\n" << add_result << "\n\n";
 
-	auto elem_wise_mul = cl_graph::Node::element_wise_mul_node(data1, data2, device);
-	auto elem_wise_scalar = cl_graph::Node::element_wise_mul_node(elem_wise_mul, scalar, device);
+    auto elem_wise_mul = cl_graph::Node::element_wise_mul_node(data1, data2, device);
+    auto elem_wise_scalar = cl_graph::Node::element_wise_mul_node(elem_wise_mul, scalar, device);
 
-	auto elem_wise_result = elem_wise_scalar.evaluate();
+    auto elem_wise_result = elem_wise_scalar.evaluate();
 
-	// output result
-	std::cout << "A matrix:\n" << data1 << '\n';
-	std::cout << ".*\n";
-	std::cout << "B matrix:\n" << data2 << '\n';
-	std::cout << "=\n";
-	std::cout << "C matrix:\n" << elem_wise_result << "\n\n";
+    // output result
+    std::cout << "A matrix:\n" << data1 << '\n';
+    std::cout << ".*\n";
+    std::cout << "B matrix:\n" << data2 << '\n';
+    std::cout << "=\n";
+    std::cout << "C matrix:\n" << elem_wise_result << "\n\n";
 
-	auto abs_data1 = cl_graph::Node::abs_node(data1, device);
-	auto abs_data2 = cl_graph::Node::abs_node(data2, device);
+    auto abs_data1 = cl_graph::Node::abs_node(data1, device);
+    auto abs_data2 = cl_graph::Node::abs_node(data2, device);
 
-	auto abs_data1_result = abs_data1.evaluate();
-	auto abs_data2_result = abs_data2.evaluate();
+    auto abs_data1_result = abs_data1.evaluate();
+    auto abs_data2_result = abs_data2.evaluate();
 
-	std::cout << "abs(A) matrix:\n" << abs_data1_result << '\n';
-	std::cout << "abs(B) matrix:\n" << abs_data2_result << '\n';
+    std::cout << "abs(A) matrix:\n" << abs_data1_result << '\n';
+    std::cout << "abs(B) matrix:\n" << abs_data2_result << '\n';
 
     auto sqrt_data1 = cl_graph::Node::sqrt_node(data1, device);
     auto sqrt_data2 = cl_graph::Node::sqrt_node(data2, device);
