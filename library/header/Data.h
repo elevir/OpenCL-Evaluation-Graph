83b71f358d91861ef1e882933113cc17e1c05cb9
Andrey Potemkin <appanpotemkin@gmail.com>
2019-02-18 23:48:19 +0300

    tabs->spaces

--- library/header/Data.h
+++ library/header/Data.h
@@ -10,39 +10,39 @@
 
 namespace cl_graph {
 
-class OPENCL_EVAL_G_API Data
-{
+class OPENCL_EVAL_G_API Data {
 public:
     Data();
     Data(const Data &) = default;
-	Data(Data &&) = default;
-	Data(std::vector<float> container, std::vector<size_t> shape);
-	template <class ...T>
-	Data(const T & ... data);
-	template <class ...T>
-	Data(std::initializer_list<T> && ... data);
-	template <class ...T>
-	Data(std::initializer_list<std::initializer_list<T>> && ... data);
+    Data(Data &&) = default;
+    Data(std::vector<float> container, std::vector<size_t> shape);
+    template<class ...T>
+    Data(const T & ... data);
+    template<class ...T>
+    Data(std::initializer_list<T> && ... data);
+    template<class ...T>
+    Data(std::initializer_list<std::initializer_list<T>> && ... data);
 
-	Data & operator=(const Data &) = delete;
+    Data & operator=(const Data &) = delete;
 
-	template <class T>
-	bool get_shaped_data(T & shaped_data);
-	template <class ...T>
-	bool set_shaped_data(const T & ... shaped_data);
-	template <class ...T, void * = nullptr>
-	bool set_shaped_data(std::initializer_list<T> && ... data);
-	template <class ...T>
-	bool set_shaped_data(std::initializer_list<std::initializer_list<T>> && ... data);
+    template<class T>
+    bool get_shaped_data(T & shaped_data);
+    template<class ...T>
+    bool set_shaped_data(const T & ... shaped_data);
 
-	bool download(std::vector<float> & data, std::vector<size_t> & shape) const;
-	bool upload(std::vector<float> data, std::vector<size_t> shape);
+    template<class ...T, void * = nullptr>
+    bool set_shaped_data(std::initializer_list<T> && ... data);
+    template<class ...T>
+    bool set_shaped_data(std::initializer_list<std::initializer_list<T>> && ... data);
 
-	bool empty() const;
+    bool download(std::vector<float> & data, std::vector<size_t> & shape) const;
+    bool upload(std::vector<float> data, std::vector<size_t> shape);
 
-	DataImpl * get_impl();
+    bool empty() const;
 
-	friend std::ostream & operator<<(std::ostream & strm, const Data & data);
+    DataImpl * get_impl();
+
+    friend std::ostream & operator<<(std::ostream & strm, const Data & data);
 
 private:
     std::shared_ptr<DataImpl> m_impl;
